8cc3de28e5e826f88b2eb1a6bb66ceda720fbc92
Xafixav <xafiworld@gmail.com>
2024-08-05 15:10:51 -0300

    :fix: separate server start from app configuration

--- app.js
+++ app.js
@@ -3,32 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = parseInt(process.env.APP_PORT_NUMBER, 10); // 10 IN HERE IS THE BASE FOR DECIMAL
 
 app.use(express.json()); // PARSE JSON body request
-
-
-async function connectToDatabase() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Connected to MongoDB');
-    } catch(err) {
-        console.error(`Failed to connect to MongoDB, ${err}`);
-    }
-}
-
-connectToDatabase();
-
 
 
 app.get('/', (_req, res) => {
     res.send('hello world');
 });
 
-
-app.listen(PORT, () => {
-    console.log(`app listening to port: ${PORT}`);
-});
+module.exports = { app };
